9a0939aa7394eb7d07e86cab41ebb548f349e379
jesusocampo1105 <jesusocampo1105@gmail.com>
2023-01-26 16:34:14 -0500

    Created server port 3000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Desde index.js");
+const express = require("express");
+const app = express()
+const port = 3000
+
+app.get("/", (req, res)=>{
+  res.send("Usando res.send")
+})
+
+app.listen(port, ()=>{
+  console.log("mi puerto es"+port)
+})
